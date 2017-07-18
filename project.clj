71cd083b8b46a9ce75d703da15bec0a9f6424eea
Josh Lehman <jalehman37@gmail.com>
2017-07-18 11:21:53 -0700

    next dev cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/drawknife "0.1.0"
+(defproject starcity/drawknife "0.2.0-SNAPSHOT"
   :description "Logging configuration & middleware for Starcity projects."
   :url "https://github.com/starcity-properties/drawknife"
   :license {:name "Eclipse Public License"
