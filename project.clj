947834b398b6d527d19a8556929ab57b548febaa
Josh Lehman <jalehman37@gmail.com>
2017-07-21 16:39:38 -0700

    0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/drawknife "0.2.0-SNAPSHOT"
+(defproject starcity/drawknife "0.2.0"
   :description "Logging configuration & middleware for Starcity projects."
   :url "https://github.com/starcity-properties/drawknife"
   :license {:name "Eclipse Public License"
