6ce303a5136782ff973078f85a9b2fbc1af3537e
Diana Negma <dianagren@gmail.com>
2019-09-25 14:55:56 -0700

    Version 1.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/drawknife "1.1.2-SNAPSHOT"
+(defproject starcity/drawknife "1.1.2"
   :description "Logging configuration & middleware for Starcity projects."
   :url "https://github.com/starcity-properties/drawknife"
   :license {:name "Eclipse Public License"
